3b1072db7ac04709c6ace201dc15e374f9fb9b7d
Bowie <marketing@letscode.com.br>
2026-07-16 23:52:29 -0300

    Adiciona atualizacao de alimentos

--- src/routes/food.routes.ts
+++ src/routes/food.routes.ts
@@ -58,3 +58,60 @@ foodRouter.post('/', requireAuth, async (req, res) => {
 
   return res.status(201).json(food);
 });
+
+foodRouter.put('/:id', requireAuth, async (req, res) => {
+  const id = Number(req.params.id);
+
+  const {
+    name,
+    caloriesPer100g,
+    carbsPer100g,
+    proteinPer100g,
+    fatPer100g,
+  } = req.body;
+
+
+  if (
+    !name ||
+    caloriesPer100g < 0 ||
+    carbsPer100g < 0 ||
+    proteinPer100g < 0 ||
+    fatPer100g < 0
+  ) {
+    return res.status(400).json({
+      error: "Dados inválidos para atualização do alimento"
+    });
+  }
+
+
+  const food = await prisma.food.findFirst({
+    where: {
+      id,
+      userId: req.userId!,
+    },
+  });
+
+
+  if (!food) {
+    return res.status(404).json({
+      error: "Alimento não encontrado"
+    });
+  }
+
+
+  const updatedFood = await prisma.food.update({
+    where: {
+      id,
+    },
+    data: {
+      name,
+      caloriesPer100g,
+      carbsPer100g,
+      proteinPer100g,
+      fatPer100g,
+    },
+  });
+
+
+  return res.json(updatedFood);
+});
